21fff9a36aee3c749997cd6593640b7ef94a6bf1
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-16 16:18:57 +0200

    feat: favorites service implementation

---
+++ src/app/core/services/favorites.service.spec.ts
@@ -0,0 +1,201 @@
+import {TestBed} from '@angular/core/testing';
+import {skip, take} from 'rxjs';
+
+import {FavoritesService} from './favorites.service';
+
+describe('FavoritesService', () => {
+	let service: FavoritesService;
+
+	beforeEach(() => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		let store: any = {};
+		const mockLocalStorage = {
+			getItem: (key: string): string => {
+				return key in store ? store[key] : null;
+			},
+			setItem: (key: string, value: string) => {
+				store[key] = `${value}`;
+			},
+			removeItem: (key: string) => {
+				delete store[key];
+			},
+			clear: () => {
+				store = {};
+			},
+		};
+		spyOn(localStorage, 'getItem').and.callFake(mockLocalStorage.getItem);
+		spyOn(localStorage, 'setItem').and.callFake(mockLocalStorage.setItem);
+		spyOn(localStorage, 'removeItem').and.callFake(mockLocalStorage.removeItem);
+		spyOn(localStorage, 'clear').and.callFake(mockLocalStorage.clear);
+
+		TestBed.configureTestingModule({
+			providers: [FavoritesService],
+		});
+
+		service = TestBed.inject(FavoritesService);
+	});
+
+	it('should have as default state an empty set', (done) => {
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(0);
+		expect(localStorage.getItem(FavoritesService.STORAGE_KEY)).toEqual('[]');
+
+		service.changes$.pipe(take(1)).subscribe((e) => {
+			expect(Array.isArray(e)).toBeTrue();
+			expect(e.length).toEqual(0);
+			done();
+		});
+	});
+
+	it('get should work', () => {
+		service.add(5);
+		service.add(3);
+		service.add(2);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(3);
+		expect(items).toContain(5);
+		expect(items).toContain(3);
+		expect(items).toContain(2);
+	});
+
+	it('get should return an immutable array', () => {
+		service.add(5);
+		service.add(3);
+		service.add(2);
+
+		const oldItems = service.get();
+		oldItems.push(9);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(3);
+		expect(items).toContain(5);
+		expect(items).toContain(3);
+		expect(items).toContain(2);
+	});
+
+	it('has should work', () => {
+		service.add(5);
+		service.add(3);
+
+		expect(service.has(5)).toBeTrue();
+		expect(service.has(3)).toBeTrue();
+		expect(service.has(2)).toBeFalse();
+	});
+
+	it('should add and emit to existing subscription', (done) => {
+		service.changes$.pipe(skip(1), take(1)).subscribe((e) => {
+			expect(Array.isArray(e)).toBeTrue();
+			expect(e.length).toEqual(1);
+			expect(e).toContain(5);
+			done();
+		});
+
+		service.add(5);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(1);
+		expect(items).toContain(5);
+	});
+
+	it('should add and emit to new subscribtion', (done) => {
+		service.add(5);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(1);
+		expect(items).toContain(5);
+
+		// last emitted value should be 5
+		service.changes$.pipe(take(1)).subscribe((e) => {
+			expect(Array.isArray(e)).toBeTrue();
+			expect(e.length).toEqual(1);
+			expect(e).toContain(5);
+			done();
+		});
+	});
+
+	it('should remove and emit to existing subscription', (done) => {
+		service.changes$.pipe(skip(4), take(1)).subscribe((e) => {
+			expect(Array.isArray(e)).toBeTrue();
+			expect(e.length).toEqual(2);
+			expect(e).toContain(3);
+			expect(e).toContain(2);
+			done();
+		});
+
+		service.add(5);
+		service.add(3);
+		service.add(2);
+		service.remove(5);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(2);
+		expect(items).toContain(3);
+		expect(items).toContain(2);
+	});
+
+	it('should remove and emit to new subscription', (done) => {
+		service.add(5);
+		service.add(3);
+		service.add(2);
+		service.remove(5);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(2);
+		expect(items).toContain(3);
+		expect(items).toContain(2);
+
+		service.changes$.pipe(take(1)).subscribe((e) => {
+			expect(Array.isArray(e)).toBeTrue();
+			expect(e.length).toEqual(2);
+			expect(e).toContain(3);
+			expect(e).toContain(2);
+			done();
+		});
+	});
+
+	it('should load the new value when storage event is emitted', (done) => {
+		localStorage.setItem(FavoritesService.STORAGE_KEY, JSON.stringify([1, 2]));
+		window.dispatchEvent(
+			new StorageEvent('storage', {
+				storageArea: localStorage,
+				key: FavoritesService.STORAGE_KEY,
+			}),
+		);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(2);
+		expect(items).toContain(1);
+		expect(items).toContain(2);
+
+		service.changes$.pipe(take(1)).subscribe((e) => {
+			expect(Array.isArray(e)).toBeTrue();
+			expect(e.length).toEqual(2);
+			expect(e).toContain(1);
+			expect(e).toContain(2);
+			done();
+		});
+	});
+
+	it('invalid json syntax should reset the state', () => {
+		localStorage.setItem(FavoritesService.STORAGE_KEY, '[1,2');
+		window.dispatchEvent(
+			new StorageEvent('storage', {
+				storageArea: localStorage,
+				key: FavoritesService.STORAGE_KEY,
+			}),
+		);
+
+		const items = service.get();
+		expect(Array.isArray(items)).toBeTrue();
+		expect(items.length).toEqual(0);
+	});
+});
---
+++ src/app/core/services/favorites.service.ts
@@ -0,0 +1,130 @@
+import {Injectable, NgZone, inject} from '@angular/core';
+import {BehaviorSubject, Observable, filter, fromEvent} from 'rxjs';
+
+import type {PicsumResourceIdType} from '../../types';
+
+@Injectable()
+export class FavoritesService {
+	static readonly STORAGE_KEY = 'favorites';
+
+	/**
+	 * Because the application can be opened in multiple tabs, I decided to make this service reactive.
+	 * Whenever this list is modified on any browser tab, the subscribers should be notified that it has been changed.
+	 */
+	changes$: Observable<PicsumResourceIdType[]>;
+	/**
+	 * This field will be defined by calling syncWithLocalStorage in constructor
+	 */
+	private items!: Set<PicsumResourceIdType>;
+	/**
+	 * Using BehaviourSubject I ensure that each subscriber will have always the latest items
+	 */
+	private changesSubject: BehaviorSubject<PicsumResourceIdType[]>;
+	private ngZone = inject(NgZone);
+
+	constructor() {
+		// For this project, we won't support SSR to meet the deadline
+
+		this.syncWithLocalStorage();
+
+		/**
+		 * Emit a immutable array of the items
+		 */
+		this.changesSubject = new BehaviorSubject(this.toImmutableArray());
+		this.changes$ = this.changesSubject.asObservable();
+
+		/**
+		 * Prevent a change detection by this listener
+		 */
+		this.ngZone.runOutsideAngular(() => {
+			/**
+			 * This event will be fired only when other page except itself modify the storage
+			 * Read more: https://developer.mozilla.org/en-US/docs/Web/API/Window/storage_event
+			 */
+			fromEvent<StorageEvent>(window, 'storage')
+				.pipe(filter((e) => e.storageArea === localStorage && e.key === FavoritesService.STORAGE_KEY))
+				.subscribe(() => {
+					this.syncWithLocalStorage();
+					this.changesSubject.next(this.toImmutableArray());
+				});
+		});
+	}
+
+	/**
+	 * Add a picture to favorites.
+	 *
+	 * @param id The Picsum Id
+	 */
+	add(id: PicsumResourceIdType): void {
+		this.items.add(id);
+		this.syncToLocalStorage();
+		this.changesSubject.next(this.toImmutableArray());
+	}
+
+	/**
+	 * Remove a picture from favorites.
+	 *
+	 * @param id The Picsum Id
+	 */
+	remove(id: PicsumResourceIdType): void {
+		this.items.delete(id);
+		this.syncToLocalStorage();
+		this.changesSubject.next(this.toImmutableArray());
+	}
+
+	/**
+	 * Get a immutable list of favorites
+	 */
+	get(): PicsumResourceIdType[] {
+		return this.toImmutableArray();
+	}
+
+	/**
+	 * This photos is in favorites?
+	 *
+	 * @param id The Picsum Id
+	 * @returns
+	 */
+	has(id: PicsumResourceIdType): boolean {
+		return this.items.has(id);
+	}
+
+	/**
+	 * Used for syncing the service state with the local storage
+	 */
+	private syncWithLocalStorage(): void {
+		const favoritesJson = localStorage.getItem(FavoritesService.STORAGE_KEY);
+
+		if (favoritesJson) {
+			try {
+				this.items = new Set(JSON.parse(favoritesJson));
+			} catch (e) {
+				console.error(e);
+				console.warn(
+					`An error has been thrown while parsing the JSON of the ${FavoritesService.STORAGE_KEY} from local storage.`,
+				);
+				console.warn('For prevent this behaviour, I reinitialized the default state of it.');
+
+				this.items = new Set();
+				this.syncToLocalStorage();
+			}
+		} else {
+			this.items = new Set();
+			this.syncToLocalStorage();
+		}
+	}
+
+	/**
+	 * Used for syncing the service state to the local storage.
+	 */
+	private syncToLocalStorage(): void {
+		localStorage.setItem(FavoritesService.STORAGE_KEY, JSON.stringify(this.toImmutableArray()));
+	}
+
+	/**
+	 * Get a immutable array of the items
+	 */
+	private toImmutableArray(): PicsumResourceIdType[] {
+		return Array.from(this.items.values());
+	}
+}
