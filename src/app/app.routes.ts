4fb90d9ce2a5902250db828a2903914ef438f9ed
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-16 20:37:52 +0200

    feat: added blank photos page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		loadComponent: () => import('./pages/photos/photos.component').then((m) => m.PhotosComponent),
+		title: 'Photos',
+	},
+];
---
+++ src/app/pages/photos/photos.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app-photos',
+	templateUrl: './photos.component.html',
+	styleUrl: './photos.component.scss',
+	standalone: true,
+})
+export class PhotosComponent {}
