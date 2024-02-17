02a9071b9ffa16536f6cd1fc5a50d8e2d43f4d0d
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-17 12:17:30 +0200

    feat: added empty favorites view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,4 +7,10 @@ export const routes: Routes = [
 		loadComponent: () => import('./pages/photos/photos.component').then((m) => m.PhotosComponent),
 		title: 'Photos',
 	},
+	{
+		path: 'favorites',
+		pathMatch: 'full',
+		loadComponent: () => import('./pages/favorites/favorites.component').then((m) => m.FavoritesComponent),
+		title: 'Favorites',
+	},
 ];
