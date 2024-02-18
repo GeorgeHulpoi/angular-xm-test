37f1611cf75a6b27ec20af7322059c023647133f
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-18 14:46:26 +0200

    perf+feat: prevent using functions in template & updated design

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import {NgOptimizedImage} from '@angular/common';
-import {ChangeDetectionStrategy, Component, Input} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, OnChanges} from '@angular/core';
 
 import type {PicsumResourceIdType} from '../../../types';
 
@@ -11,12 +11,14 @@ import type {PicsumResourceIdType} from '../../../types';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [NgOptimizedImage],
 })
-export class CardComponent {
+export class CardComponent implements OnChanges {
 	@Input({required: true}) id!: PicsumResourceIdType;
 
-	getImageSrc(): string {
+	src!: string;
+
+	ngOnChanges(): void {
 		// The image will be rendered at 300x300, but because usually
 		// pixel density is 2, we multiply the image size with 2
-		return `https://picsum.photos/id/${this.id}/600/600`;
+		this.src = `https://picsum.photos/id/${this.id}/600/600`;
 	}
 }
