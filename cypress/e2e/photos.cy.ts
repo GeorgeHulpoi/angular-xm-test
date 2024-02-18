3c202631cff02b9355a35e2ff7a9b9198a3bc60d
George-Valentin Hulpoi <george.v.hulpoi@gmail.com>
2024-02-18 17:17:09 +0200

    test: e2e test for photos view

---
+++ cypress/e2e/photos.cy.ts
@@ -0,0 +1,50 @@
+describe('Photos View', () => {
+	it('should load', () => {
+		cy.intercept('GET', 'https://picsum.photos/v2/list?*page=1*', {
+			fixture: 'photos/page1.json',
+		});
+
+		cy.visit('http://localhost:4200');
+		cy.get('app-card').should('have.length', 6);
+	});
+
+	it('should load more on scrolling down', () => {
+		cy.intercept('GET', 'https://picsum.photos/v2/list?*page=1*', {
+			headers: {
+				link: '<https://picsum.photos/v2/list?page=2&limit=12>; rel="next"',
+			},
+			fixture: 'photos/page1.json',
+		});
+
+		cy.intercept('GET', 'https://picsum.photos/v2/list?*page=2*', {
+			headers: {
+				link: '<https://picsum.photos/v2/list?page=1&limit=12>; rel="prev"',
+			},
+			fixture: 'photos/page2.json',
+		});
+
+		cy.visit('http://localhost:4200');
+		cy.scrollTo('bottom');
+		cy.get('app-card').should('have.length', 12);
+		cy.get('app-loader').should('not.exist');
+	});
+
+	it('should add to favorites', () => {
+		cy.intercept('GET', 'https://picsum.photos/v2/list?*page=1*', {
+			fixture: 'photos/page1.json',
+		});
+
+		cy.visit('http://localhost:4200');
+
+		const card = cy.get('app-card').first();
+		card.click();
+		card.children('img')
+			.first()
+			.invoke('attr', 'src')
+			.then((src) => {
+				cy.visit('http://localhost:4200/favorites');
+
+				cy.get('app-card').first().children('img').first().invoke('attr', 'src').should('eq', src);
+			});
+	});
+});
